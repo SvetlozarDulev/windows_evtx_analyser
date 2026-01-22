cb92348e6eb601fc75601e3cf70d97eee83fdf00
Svetlozar Dulev <pr_hack@proton.me>
2026-01-22 22:41:54 +0200

    Detection for failed logons

--- src/analyser_evtx.py
+++ src/analyser_evtx.py
@@ -3,8 +3,10 @@ from Evtx.Evtx import Evtx
 #Getting fields from XML
 import xml.etree.ElementTree as ET
 #()init ensures colors work in Windows console; Fore - text color;
-from colorama import init, Fore, Style
+from colorama import Fore, Style
 
+FAILED_THRESHOLD = 5
+failed_logons = {}
 #Namespace
 ns = {"e": "http://schemas.microsoft.com/win/2004/08/events/event"}
 def parse(file_path):
@@ -22,11 +24,16 @@ def parse(file_path):
             event_id = root.find(".//e:EventID",ns)
             time_created = root.find(".//e:TimeCreated",ns).get("SystemTime")
             process_name = root.find(".//e:Data[@Name='ProcessName']",ns)
+            username = root.find(".//e:Data[@Name='TargetUserName']",ns)
+            user_elem = username.text if username is not None else "N/A"
             process_elem = process_name.text if process_name is not None else "No process"
             if event_id.text in SECURITY_EVENTS:
                 if event_id.text == "4625":
+                    failed_logons[user_elem] = failed_logons.get(user_elem,0) + 1
                     #Style.RESET_ALL - resets color after printing
-                    print(Fore.RED + f"[FAILED LOGON] EventID: {event_id.text} | Timecreated: {time_created} | Process: {process_elem}" + Style.RESET_ALL)
+                    print(f"[FAILED LOGON] EventID: {event_id.text} | TargetUserName: {user_elem} | Timecreated: {time_created} | Process: {process_elem}")
+                    if failed_logons[user_elem] == FAILED_THRESHOLD:
+                        print(Fore.RED + f"[ALERT] Possible BRUTE-FORCE on user {user_elem}" + Style.RESET_ALL)
                 elif event_id.text == "4624":
                     ...
                 elif event_id.text == "4776":
